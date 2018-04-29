370592421162f796f9a65f6a1accec897bc6ea50
Jake Bolam <jake.bolam@gmail.com>
2018-04-28 21:18:54 -0400

    First commit

---
+++ config.bundlesize.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "files": [
+        {
+            "path": "./__testdata__/*.jpg",
+            "maxSize": "500kB"
+        },
+        {
+            "path": "./README.md",
+            "maxSize": "5kB"
+        }
+    ]
+}
